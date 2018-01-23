b2315a944b563ca92af701868ce21fe0d9fa60e7
Antonella Morittu <morittuantonella@gmail.com>
2018-01-23 14:49:24 +0100

    adding insufficient funds test

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,8 +5,18 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount)
-    @funds -= amount
+  def withdraw(amount, account)
+  case
+  when insufficient_funds_in_account?(amount, account)
+    { status: false, message: 'insufficient funds', date: Date.today }
+  else
+    perform_transaction(amount, account)
   end
+end
 
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
 end
