bee5e472ee7c0e87f5a9fd2d755deaf341aceade
Lara Thordardottir <lara-thordar@hotmail.com>
2018-01-23 14:26:40 +0100

    errors fixed

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,48 +5,14 @@ describe Atm do
   let(:account) { instance_double('Account') }
 
   before do
-  allow(account).to receive(:balance).and_return(100)
-  allow(account).to receive(:balance=)
-  end
+    allow(account).to receive(:balance).and_return(100)  
+    allow(account).to receive(:balance=)
+end
 
-  it 'has 1000$ on initialize' do
-    expect(subject.funds).to eq 1000
-  end
+
 
   it 'funds are reduced at withdraw' do
     subject.withdraw(50, account)
     expect(subject.funds).to eq 950
   end
 end
-
-=begin
-   it 'allow withdraw if account has enough balance.' do
-     expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-     expect(subject.withdraw(45, account)).to eq expected_output
-    end
-=end
-
-=begin
-  describe Atm do
-    let(:account) { instance_double('Account') }
-    before
-    allow(account).to receive(:balance).and_return(100)
-    allow(account).to receive(:balance=)
-  end
-end
-end
-=end
-
-=begin
-  before do
-    allow(account).to receive(:balance).and_return(100)
-    allow(account).to receive(:balance=)
-  end
-
-  describe Atm do
-    it 'allow withdraw if account has enough balance.' do
-      expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-      expect(subject.withdraw(45, account)).to eq expected_output
-    end
-  end
-=end
