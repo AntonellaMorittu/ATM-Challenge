4f44906b2bc93524f7095f6c9cb58b6b6b662767
Lara Thordardottir <lara-thordar@hotmail.com>
2018-01-23 15:57:24 +0100

    Testing funds

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,12 +5,9 @@ describe Atm do
   let(:account) { instance_double('Account') }
 
   before do
-    allow(account).to receive(:balance).and_return(100)  
+    allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
-end
-
-
-
+    
   it 'funds are reduced at withdraw' do
     subject.withdraw(50, account)
     expect(subject.funds).to eq 950
