c1dd88950f75985a4def49a497936214d495d2de
Antonella Morittu <morittuantonella@gmail.com>
2018-01-25 15:41:01 +0100

    withdraw method added withdraw funds method added increase_cash method added missing_atm method added

--- lib/account.rb
+++ lib/account.rb
@@ -7,26 +7,28 @@ class Account
   STANDARD_VALIDITY_YRS = 5
 
   def initialize(args ={})
-    @pin_code = pin_code
+    @pin_code = create_pin_code
     @balance = 0
     @exp_date = set_exp_date
     @account_status = :active
     @owner = set_owner(args[:owner])
   end
 
-  def pin_code
-    rand(1000..9999)
-  end
 
-  def set_exp_date
-    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
-  end
 
   def self.deactivate(account)
     account.account_status = :deactivated
   end
 
   private
+
+  def create_pin_code
+    rand(1000..9999)
+  end
+
+  def set_exp_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+  end
 
   def set_owner(obj)
     obj.nil? ? missing_owner : @owner = obj
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,6 @@
-require './lib/atm.rb'
 require './lib/account.rb'
+require './lib/atm'
+require 'pry'
 
 class Person
   #information stored in the class we want to access
@@ -7,11 +8,13 @@ class Person
 # initialize is a method needed to take some actions,
 # instances can be considered as copies of the class(in this case
 # the person is our class)
-def initialize(arg={})#initialize takes arguments, information stored in between {}
-    #calling and instance allow us to set a name and
-    #match it with the name we have in the test(Thomas or Antonella in this case)
-    @name = set_name(arg[:name])
-    @cash = 0
+
+def initialize(args={})#initialize takes arguments, information stored in between {}
+  #calling and instance allow us to set a name and
+  #match it with the name we have in the test(Thomas or Antonella in this case)
+  @name = set_name(args[:name])
+  @cash = 0
+  @account = nil
 end
 
 #ternary operator (line 21) a ? b : c
@@ -24,25 +27,46 @@ end
 #here we have to define the missing_name method to return a message
 def missing_name
   raise "A name is required"
-  end
+end
 
 #define the function create_account
 def create_account()
-    #the account can be created (Account.new) with the subject(self) as owner
-    @account = Account.new(owner: self)
-  end
+  #the account can be created (Account.new) with the subject(self) as owner
+  @account = Account.new(owner: self)
+end
+
+def deposit_funds(amount)
+  @account.balance += amount
+  @cash -= amount
+end
 
 def deposit(amount)
-    @account.balance += amount
-    @cash -= amount
-  end
-
-def deposit(amount)
-    account.nil? ? missing_account : @account = account
-  end
+  @account.nil? ? missing_account : deposit_funds(amount)
+end
 
 def missing_account
   raise "No account present"
-  end
+end
+
+def withdraw(args = {})
+  @account == nil ? missing_account : withdraw_funds(args)
+end
+
+def withdraw_funds(args)
+  args[:atm] == nil ? missing_atm : atm = args[:atm]
+  account = @account
+  amount = args[:amount]
+  pin = args[:pin]
+  response = atm.withdraw(amount, pin, account)
+  response[:status] == true ? increase_cash(response) : response
+end
+
+def increase_cash(response)
+  @cash += response[:amount]
+end
+
+def missing_atm
+  raise RuntimeError, 'An ATM is required'
+end
 
 end
