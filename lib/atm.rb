4838c3852791134857bbb9f348abc8a5081c71d3
Antonella Morittu <morittuantonella@gmail.com>
2018-01-26 13:38:30 +0100

    script beautified

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,15 +8,16 @@ class Atm
   end
 
   def withdraw(amount, pin_code, account)
-    if insufficient_funds_in_account?(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds in account', date: Date.today }
-    elsif insufficient_funds_in_atm?(amount)
+    when insufficient_funds_in_atm?(amount)
       { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    elsif incorrect_pin?(pin_code, account.pin_code)
+    when incorrect_pin?(pin_code, account.pin_code)
       { status: false, message: 'wrong pin', date: Date.today }
-    elsif card_expired?(account.exp_date)
+    when card_expired?(account.exp_date)
       { status: false, message: 'card expired', date: Date.today }
-    elsif account_inactive?(account.account_status)
+    when account_inactive?(account.account_status)
       { status: false, message: 'account inactive', date: Date.today }
     else
       perform_transaction(amount, account)
@@ -54,8 +55,12 @@ class Atm
 
   def perform_transaction(amount, account)
     @funds -= amount
-    account.balance = account.balance - amount
-    { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
+    account.balance -= amount
+    { status: true,
+      message: 'success',
+      date: Date.today,
+      amount: amount,
+      bills: add_bills(amount) }
 end
 
   def add_bills(amount)
